0b4c32df1f0e25c69948399a2397e6ea7c01dad2
JoaoGuiMachado <joao.gmachado14@gmail.com>
2024-07-24 19:02:18 -0300

    Teste

--- __pycache__/selecaoAmostral.py
+++ __pycache__/selecaoAmostral.py
@@ -151,7 +151,7 @@ while True:
                                             celula = sheet.cell(rowReserva, column)
                                             print(celula.value)
                                             rowReserva = rowReserva + 1
-                                            #TIRAR NONE, ainda aparece ele
+                                            #TIRAR NONE, ainda aparece ele. ACHAR FORMA DE TIRAR
                                 #Criar função para realocar os itens na planilha da seleção amostral
                             if valor_string == "SERVIÇO DE TERCEIROS" or valor_string == "SERVIÇOS DE TERCEIROS":
                                 print(celula.value)
